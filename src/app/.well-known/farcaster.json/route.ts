81f6ebe67965ec47fff53ac68a6875ee9c4ef88f
macnelson9 <michaelofatu@gmail.com>
2025-11-24 19:32:48 +0100

    update farcaster manifest content

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,16 +1,33 @@
-function withValidProperties(
-  properties: Record<string, undefined | string | string[]>
-) {
-  return Object.fromEntries(
-    Object.entries(properties).filter(([_, value]) =>
-      Array.isArray(value) ? value.length > 0 : !!value
-    )
-  );
-}
-
 export async function GET() {
-  return Response.redirect(
-    "https://api.farcaster.xyz/miniapps/hosted-manifest/0198fd14-6be3-fd68-3202-b459d18a4433",
-    308
-  );
+  return Response.json({
+    frame: {
+      name: "GuessQuest",
+      version: "1",
+      iconUrl: "https://guessquest-seven.vercel.app/icon.png",
+      homeUrl: "https://guessquest-seven.vercel.app",
+      imageUrl: "https://guessquest-seven.vercel.app/image.png",
+      buttonTitle: "Open GuessQuest",
+      splashImageUrl:
+        "https://guessquest-seven.vercel.app/.well-known/farcaster.json/image.png",
+      splashBackgroundColor: "#000000",
+      webhookUrl: "https://guessquest-seven.vercel.app/api/webhook",
+      subtitle: "A fun guessing game",
+      description: "Guess the answer to win!",
+      primaryCategory: "games",
+      tags: ["games", "social", "fun", "casual", "exciting"],
+      heroImageUrl:
+        "https://guessquest-seven.vercel.app/.well-known/farcaster.json/hero.png",
+      tagline: "Play instantly, win together",
+      ogTitle: "GuessQuest - Fast, fun, social",
+      ogDescription: "Fast, fun, social",
+      ogImageUrl: "https://guessquest-seven.vercel.app/hero.png",
+    },
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjEzMTAzMDcsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhDM2UwRjA3NTAzQUIwMTA5OEI0YTkwNzc2N0M2NWY1NDljODJBOGU3In0",
+      payload: "eyJkb21haW4iOiJndWVzc3F1ZXN0LXNldmVuLnZlcmNlbC5hcHAifQ",
+      signature:
+        "BnuHpqIKAFF7Z36sK2vm0+YfMLxWCbZSRZxluke9q9439TYNEmfYTyyB3GrcdQ8XP3xkseqES5lD/1g3r+kGvxw=",
+    },
+  });
 }
